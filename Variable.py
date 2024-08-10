187f95594cd308dec20913ebdde345eb7e512ae8
AbdulMoizAbbasi <moizabbasi123@gmail.com>
2024-08-10 06:19:22 +0500

    Initial commit

---
+++ Variable.py
@@ -0,0 +1,12 @@
+Start = "Starting this journey by the name of Allah"  # every var has a value associated with it.
+print(Start)
+
+Start = "Bismillah"
+print(Start)
+
+# Variable Naming convention
+_var1 = "Moiz"
+var2 = "Moiz"
+# 3var = "Moiz" Incorrect
+# var 4 = "Moiz" Incorrect
+
