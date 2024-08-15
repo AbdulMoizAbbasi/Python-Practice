3f61dfea942c90fd7bf6ae59e0e0904be4cc5c4f
AbdulMoizAbbasi <121189033+AbdulMoizAbbasi@users.noreply.github.com>
2024-08-15 15:05:24 +0500

    Few updates

--- Numbers.py
+++ Numbers.py
@@ -1,2 +1,9 @@
 print(3*2)
 print(3**2) #exponential
+print(4/2) #dividing any two num you will get a float
+print(5/2)
+payment = 24_000_000
+print(payment) # output = 24000000
+x, y, z = 0, 0, 0
+print(f"{x}\n{y}\n{z}")
+AGE = 10 # all capital letters for constants, that shouldn't be changed throughout the program
