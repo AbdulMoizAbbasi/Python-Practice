7f0124d73da90b0cc3d7f467215df12a8200f5ae
AbdulMoizAbbasi <121189033+AbdulMoizAbbasi@users.noreply.github.com>
2024-08-16 23:15:51 +0500

    Few updates

--- Lists.py
+++ Lists.py
@@ -1,4 +1,11 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+bicycles[0] = bicycles[0].title()
 print(bicycles[-3])
 print(bicycles[-1:-4])
 print(bicycles[0:3])
+print(bicycles) #??
+print(type(bicycles))
+
+message = f"I have a {bicycles[0]} that looks exactly same as {bicycles[-2]}"
+print(message)
+print(type(message))
