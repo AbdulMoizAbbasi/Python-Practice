6ddd5b1e39b7908b1541b34a2380a14916f6aac2
AbdulMoizAbbasi <121189033+AbdulMoizAbbasi@users.noreply.github.com>
2024-08-17 06:48:21 +0500

    Few updates

--- Lists.py
+++ Lists.py
@@ -9,3 +9,28 @@ print(type(bicycles))
 message = f"I have a {bicycles[0]} that looks exactly same as {bicycles[-2]}"
 print(message)
 print(type(message))
+
+bicycles.insert(3,"sdfa")
+print(bicycles)
+
+bicycles.append("Honda")
+print(bicycles)
+
+del bicycles[1]
+print(f"{bicycles} Delete function")
+
+del bicycles[2]
+print(bicycles)
+
+x = bicycles.pop(2)
+print(bicycles)
+print(x)
+
+bicycles.remove("Trek")
+print(bicycles)
+
+bicycles.reverse()
+print(bicycles)
+
+bicycles.clear()
+print(bicycles)
