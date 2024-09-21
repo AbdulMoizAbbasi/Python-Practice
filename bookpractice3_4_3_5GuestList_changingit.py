85b2904853b1b2268c73d99bef55231b04a0efe2
AbdulMoizAbbasi <121189033+AbdulMoizAbbasi@users.noreply.github.com>
2024-09-21 15:54:45 +0500

    Few updates

--- bookpractice3_4_3_5GuestList_changingit.py
+++ bookpractice3_4_3_5GuestList_changingit.py
@@ -1,4 +1,7 @@
 guests = ["ali", "ahmed", "umer"]
+guests.insert(0, "sdf")
+guests.insert(2, "john")
+guests.append("john")
 
 for i in guests:
     print(f"Hey! {i}\nYou are invited for the dinner.")
@@ -9,3 +12,8 @@ guests[index] = "usman"
 
 for i in guests:
     print(f"Hey! {i}\nYou are invited for the dinner.")
+print(len(guests))
+
+while len(guests) == 2:
+    x = guests.pop()
+    print(f"Hey! {x} I'm sorry I can’t invite you to dinner.")
